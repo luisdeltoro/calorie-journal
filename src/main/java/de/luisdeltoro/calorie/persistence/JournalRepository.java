c7387ae9159e13ebcfb8837379ea0cf03e94e4d0
Luis del Toro <luisdeltoro@gmail.com>
2016-01-26 18:16:49 +0100

    Make use of specific JpaRepository

--- src/main/java/de/luisdeltoro/calorie/persistence/JournalRepository.java
+++ src/main/java/de/luisdeltoro/calorie/persistence/JournalRepository.java
@@ -1,7 +1,7 @@
 package de.luisdeltoro.calorie.persistence;
 
 import de.luisdeltoro.calorie.model.Journal;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -9,7 +9,7 @@ import java.util.UUID;
 /**
  * Provides CRUD operations for the {@link Journal} entity
  */
-public interface JournalRepository extends CrudRepository<Journal, Long> {
+public interface JournalRepository extends JpaRepository<Journal, Long> {
 
     /**
      * Finds a calorie journal matching the provided business identifier
